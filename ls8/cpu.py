ad9f49fead3a684434c76792c8ef961e37db65e1
HP <martinsonyedikachi@gmail.com>
2020-03-24 22:05:30 +0100

    Refactored CPU file for reading ls8 files

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -1,5 +1,4 @@
 """CPU functionality."""
-
 import sys
 
 
@@ -8,50 +7,71 @@ class CPU:
 
     def __init__(self):
         """Construct a new CPU."""
-
         self.ram = [0] * 256
-        # General Registers
         self.reg = [0] * 8
-
-        # Internal Registers
         self.PC = 0
-        self.IR = None
-        self.MAR = None
-        self.MDR = None
-        self.FL = 00000000
+        self.flag = 0
+        self.HALTED = False
 
     def ram_read(self, address):
-        return self.ram[int(address)]
+        return self.ram[address]
 
-    def ram_write(self, address, value):
-        self.ram[int(address)] = value
+    def raw_write(self, address, value):
+        self.ram[address] = value
 
     def load(self):
         """Load a program into memory."""
-
         address = 0
 
         # For now, we've just hardcoded a program:
 
-        program = [
-            # From print8.ls8
-            0b10000010,  # LDI R0,8
-            0b00000000,
-            0b00001000,
-            0b01000111,  # PRN R0
-            0b00000000,
-            0b00000001,  # HLT
-        ]
+        # program = [
+        #     # From print8.ls8
+        #     0b10000010, # LDI R0,8
+        #     0b00000000,
+        #     0b00001000,
+        #     0b01000111, # PRN R0
+        #     0b00000000,
+        #     0b00000001, # HLT
+        # ]
+        # address = 0
 
-        for instruction in program:
-            self.ram[address] = instruction
-            address += 1
+        if len(sys.argv) != 2:
+            print("usage: ls8.py <filename>")
+            sys.exit(1)
+
+        try:
+            with open(sys.argv[1]) as instructions:
+                # read each instruction line
+                for line in instructions:
+                    # split each line into instructions and comments
+                    split_instruction_line = line.split("#")
+
+                    # remove whitespace
+                    nums = split_instruction_line[0].strip()
+
+                    # ignore blank lines / comment only lines
+                    if len(nums) == "":
+                        continue
+
+                    # set the number to an integer of base 2
+                    instruction = int(nums, 2)
+                    self.ram[address] = instruction
+                    address += 1
+
+        except FileNotFoundError:
+            print(f"{sys.argv[0]}: {sys.argv[1]} not found")
+            sys.exit(2)
 
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
 
+        # for add operation
         if op == "ADD":
             self.reg[reg_a] += self.reg[reg_b]
+            # for mulitplication operation
+        elif op == "MUL":
+            self.reg[reg_a] *= self.reg[reg_b]
         # elif op == "SUB": etc
         else:
             raise Exception("Unsupported ALU operation")
@@ -61,7 +81,6 @@ class CPU:
         Handy function to print out the CPU state. You might want to call this
         from run() if you need help debugging.
         """
-
         print(f"TRACE: %02X | %02X %02X %02X |" % (
             self.PC,
             # self.fl,
@@ -70,34 +89,30 @@ class CPU:
             self.ram_read(self.PC + 1),
             self.ram_read(self.PC + 2)
         ), end='')
-
         for i in range(8):
             print(" %02X" % self.reg[i], end='')
-
         print()
 
     def run(self):
-        running = True
-
+        """Run the CPU."""
         LDI = 0b10000010
         PRN = 0b01000111
         HLT = 0b00000001
+        MUL = 0b10100010
 
-        while running:
-            IR = self.ram_read(self.PC)
+        while not self.HALTED:
+            IR = self.ram[self.PC]
             operand_a = self.ram_read(self.PC + 1)
             operand_b = self.ram_read(self.PC + 2)
-
-            if IR == LDI:
-                self.reg[operand_a] = operand_b
-                self.PC += 3
-
-            if IR == PRN:
-                print(operand_a)
-                print(self.reg[operand_a])
-                self.PC += 2
+            operands = (IR & 0b11000000) >> 6
 
             if IR == HLT:
-                self.PC += 1
-                print("exiting....")
-                running = False
+                print("Exiting...")
+                self.HALTED = True
+            elif IR == LDI:
+                self.reg[operand_a] = operand_b
+            elif IR == PRN:
+                print(self.reg[operand_a])
+            elif IR == MUL:
+                self.alu("MUL", operand_a, operand_b)
+            self.PC += operands + 1
